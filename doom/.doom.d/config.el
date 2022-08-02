241167d3ee2f305b16bcba39070f60790473c9d9
Ismael Jaras <is.jaras@gmail.com>
2022-08-02 11:47:02 -0400

    add custom company config

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -113,3 +113,40 @@
   :ensure t)
 ;; enable for all buffers
 (global-wakatime-mode)
+
+
+;; custom company config
+(setq company-minimum-prefix-length 1
+    company-tooltip-limit 4
+    company-tooltip-minimum 4
+    company-tooltip-align-annotations t
+    company-tooltip-flip-when-above t
+    company-require-match 'never
+    company-global-modes
+    '(not erc-mode
+          message-mode
+          help-mode
+          gud-mode
+          vterm-mode)
+    company-frontends
+    '(company-pseudo-tooltip-frontend  ; always show candidates in overlay tooltip
+      company-echo-metadata-frontend)  ; show selected candidate docs in echo area
+
+    ;; Buffer-local backends will be computed when loading a major mode, so
+    ;; only specify a global default here.
+    company-backends '(company-capf)
+
+    ;; These auto-complete the current selection when
+    ;; `company-auto-commit-chars' is typed. This is too magical. We
+    ;; already have the much more explicit RET and TAB.
+    company-auto-commit nil
+
+    ;; Only search the current buffer for `company-dabbrev' (a backend that
+    ;; suggests text your open buffers). This prevents Company from causing
+    ;; lag once you have a lot of buffers open.
+    company-dabbrev-other-buffers nil
+    ;; Make `company-dabbrev' fully case-sensitive, to improve UX with
+    ;; domain-specific words with particular casing.
+    company-dabbrev-ignore-case nil
+    company-dabbrev-downcase nil
+    company-idle-delay 0)  ; No delay in showing suggestions
