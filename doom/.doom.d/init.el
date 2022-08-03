b9cca0c18877d898146c0ebd2f0ac276eeb0fc4d
Ismael Jaras <is.jaras@gmail.com>
2022-08-03 18:21:50 -0400

    include company childframe And use ivy instead of vertico

--- doom/.doom.d/init.el
+++ doom/.doom.d/init.el
@@ -20,11 +20,14 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       company           ; the ultimate code completion backend
+       company
+       (company +childframe)  ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
-       vertico           ; the search engine of the future
+       (ivy +prescient -childframe
+      -fuzzy +icons)       ; a search engine for love and life
+       ;;vertico           ; the search engine of the future
 
        :ui
        ;;deft              ; notational velocity for Emacs
