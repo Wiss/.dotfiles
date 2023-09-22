36b91719ff6fec2563efecd3036eb5508b947e7f
Ismael Jaras <is.jaras@gmail.com>
2023-09-22 21:06:29 +0200

    include roam2 and more beautiful python

--- doom/.doom.d/init.el
+++ doom/.doom.d/init.el
@@ -152,13 +152,12 @@
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
         (org +pretty +journal -dragndrop
-        +hugo +roam +pandoc
+        +hugo +roam2 +pandoc
         +present)          ; organize your plain life in plain text
-       org               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       python            ; beautiful is better than ugly
+       (python +lsp +peek +pyright)       ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
