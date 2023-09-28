e97ae9bfeb829e1b91656ccd4eef963649864695
Ismael Jaras <is.jaras@gmail.com>
2023-09-28 20:25:31 +0200

    use vertico instead of ivy

--- doom/.doom.d/init.el
+++ doom/.doom.d/init.el
@@ -24,16 +24,16 @@
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
-       (ivy +prescient -childframe
-      -fuzzy +icons)       ; a search engine for love and life
-       ;;vertico           ; the search engine of the future
+       ;;(ivy +prescient -childframe
+      ;;-fuzzy +icons)       ; a search engine for love and life
+       (vertico +icons)           ; the search engine of the future
 
        :ui
        ;;deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
-       (emoji +unicode)  ; 🙂
+       ;;(emoji +unicode)  ; 🙂
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
        indent-guides     ; highlighted indent columns
