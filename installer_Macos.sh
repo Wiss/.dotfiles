1a0374b4539b7eb5b5fb3f958c74878d905d785d
Ismael Jaras <ijaras@staff-200-108.eduroam.unibe.ch>
2023-10-25 19:58:14 +0200

    include macos installer

---
+++ installer_Macos.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+
+BASEDIR="$(cd "$(dirname "${0}")" && pwd)"
+
+# bashrc
+ln -vs ${BASEDIR}/.dotfiles/bash/.bashrc ${BASEDIR}/.bashrc
+
+# vim
+ln -vs ${BASEDIR}/.dotfiles/vim/.vimrc ${BASEDIR}/.vimrc
+ln -vs ${BASEDIR}/.dotfiles/vim/.vim ${BASEDIR}/.vim
+
+# doom
+# Remember: 
+# 1. Install emacs
+# 2. clone doom repository 
+# 3. doom install 
+# link files here
+ln -vs ${BASEDIR}/.dotfiles/doom/.doom.d ${BASEDIR}/.doom.d
+
+# git
+ln -vs ${BASEDIR}/.dotfiles/git/.gitconfig ${BASEDIR}/.gitconfig
