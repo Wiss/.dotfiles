f8e5cf51de2e16a6b39ddeb60eedd65092421366
Ismael Jaras <is.jaras@gmail.com>
2023-10-25 20:49:46 +0200

    check files existence

--- installer_Macos.sh
+++ installer_Macos.sh
@@ -4,10 +4,13 @@
 BASEDIR=$(pwd)
 
 # bashrc
+[ -d "${BASEDIR}/.bashrc" ] && rm -r "${BASEDIR}/.bashrc" 
 ln -vs ${BASEDIR}/.dotfiles/bash/.bashrc ${BASEDIR}/.bashrc
 
 # vim
+[ -d "${BASEDIR}/.vimrc" ] && rm -r "${BASEDIR}/.vimrc" 
 ln -vs ${BASEDIR}/.dotfiles/vim/.vimrc ${BASEDIR}/.vimrc
+[ -d "${BASEDIR}/.vim" ] && rm -r "${BASEDIR}/.vim" 
 ln -vs ${BASEDIR}/.dotfiles/vim/.vim ${BASEDIR}/.vim
 
 # doom
@@ -16,7 +19,9 @@ ln -vs ${BASEDIR}/.dotfiles/vim/.vim ${BASEDIR}/.vim
 # 2. clone doom repository 
 # 3. doom install 
 # link files here
+[ -d "${BASEDIR}/.doom.d" ] && rm -r "${BASEDIR}/.doom.d" 
 ln -vs ${BASEDIR}/.dotfiles/doom/.doom.d ${BASEDIR}/.doom.d
 
 # git
+[ -d "${BASEDIR}/.gitconfig" ] && rm -r "${BASEDIR}/.gitconfig" 
 ln -vs ${BASEDIR}/.dotfiles/git/.gitconfig ${BASEDIR}/.gitconfig
