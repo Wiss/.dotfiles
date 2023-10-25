71d40a2aa3273d4c68ec5d485d63505666f4d8eb
Ismael Jaras <is.jaras@gmail.com>
2023-10-25 20:47:39 +0200

    improve doom config

--- doom/.doom.d/custom.el
+++ doom/.doom.d/custom.el
@@ -6,6 +6,8 @@
  '(custom-safe-themes
    '("02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" default))
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(org-safe-remote-resources
+   '("\\`https://fniessen\\.github\\.io/org-html-themes/org/theme-readtheorg\\.setup\\'"))
  '(package-selected-packages
    '(auto-complete platformio-mode ccls wakatime-mode writegood-mode langtool org-ref))
  '(safe-local-variable-values '((eval progn (pp-buffer) (indent-buffer))))
@@ -25,7 +27,7 @@
      (doom-after-init-hook)
      (doom-after-init-hook)
      (doom-after-init-hook)
-     (defvaralias)) t))
+     (defvaralias))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
