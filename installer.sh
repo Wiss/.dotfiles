1177bf8c06d0dfef2767c87c35ebff5a1fed5bc6
Ismael Jaras <is.jaras@gmail.com>
2024-01-18 18:53:18 +0100

    change BASEDIR & fix path for new emacs/doom path

--- installer.sh
+++ installer.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
+#BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+#echo $BASH_SHOURCE[0]
+#echo $BASH_SHOURCE
+#echo $BASEDIR
+#echo $(pwd)
+BASEDIR=$(pwd)
+# If using pwd as the BASEDIR, then this file should be run from
+# the $HOME path
 
 # bashrc
 [ -f "${BASEDIR}/.bashrc" ] && rm "${BASEDIR}/.bashrc"
@@ -19,8 +25,10 @@ ln -vs ${BASEDIR}/.dotfiles/vim/.vim ${BASEDIR}/.vim
 # 2. clone doom repository 
 # 3. doom install 
 # link files here
-[ -d "${BASEDIR}/.config/.doom.d" ] && rm -r "${BASEDIR}/.config/.doom.d"
-ln -vs ${BASEDIR}/.dotfiles/doom/.doom.d ${BASEDIR}/.config/.doom.d
+#[ -d "${BASEDIR}/.config/.doom.d" ] && rm -r "${BASEDIR}/.config/.doom.d"
+#ln -vs ${BASEDIR}/.dotfiles/doom/.doom.d ${BASEDIR}/.config/.doom.d
+[ -d "${BASEDIR}/.config/doom" ] && rm -r "${BASEDIR}/.config/doom"
+ln -vs ${BASEDIR}/.dotfiles/doom/.doom.d ${BASEDIR}/.config/doom
 
 # git
 [ -f "${BASEDIR}/.gitconfig" ] && rm "${BASEDIR}/.gitconfig"
