a3694d9f5c247cdba3b426219353ca95b92b9e45
Ismael Jaras <is.jaras@gmail.com>
2024-03-12 14:51:08 +0100

    include copilo

--- doom/.doom.d/packages.el
+++ doom/.doom.d/packages.el
@@ -16,8 +16,9 @@
 (unpin! org-roam)
 (package! org-roam-ui)
 
-;;(package! copilot
-;;  :recipe (:host github :repo "zerolfx/copilot.el" :files ("*.el" "dist")))
+(package! copilot
+  :recipe (:host github :repo "copilot-emacs/copilot.el" :files ("*.el" "dist")))
+
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
